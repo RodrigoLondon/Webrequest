6ddafb1fe166de920b90262567048934039a916c
Lewis Jones <apple@apples-macbook.home>
2016-11-13 21:42:41 +0000

    added files

--- Webrequest/ViewController.swift
+++ Webrequest/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView)
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        loadRequest("http://www.buzzfeed.com/")
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+        func loadRequest(urlStr: String) {
+            
+            let url = NSURL(string: urlStr)!
+            let request = NSURLRequest(URL: url)
+            webView.loadRequest(request)
+        
+        
+        
+    }
 
 
+    @IBAction func loadNetflix(sender: AnyObject) {
+    loadRequest("http://www.netflix.com/")
+    }
+    @IBAction func loadBuzzfeed(sender: AnyObject) {
+        loadRequest("http://www.buzzfeed.com/")
+
+    }
+    
+    @IBAction func loadFacebook(sender: AnyObject) {
+        loadRequest("https://www.facebook.com/")
+    }
 }
-
